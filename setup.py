4b74a90d57757e0efb8b8ef0aa0deb05ca91af17
Ryan Harter <harterrt@gmail.com>
2017-02-28 22:34:47 +0000

    Format project as a package

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='BETL',
+      version='1.0',
+      description='Basic ETL for Firefox Telemetry Pings',
+      author='Ryan Harter (harterrt)',
+      author_email='harterrt@mozilla.com',
+      url='https://github.com/harterrt/betl',
+      py_modules=['python.transform'],
+     )
